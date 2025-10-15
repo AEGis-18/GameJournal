a83f23338dab23bb5647dcc026bee8e6ff80e8db
Agustin Escobar <agusesgi@gmail.com>
2025-10-15 19:42:02 -0300

    Create project

---
+++ src/test/java/agus/gamelist/GameListApplicationTests.java
@@ -0,0 +1,13 @@
+package agus.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
