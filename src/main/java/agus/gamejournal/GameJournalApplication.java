9cce40a737defab95ed0ef3393aa1b3a4e038a6b
Agustin Escobar <agusesgi@gmail.com>
2025-11-12 18:15:13 -0300

    Rename application file

--- src/main/java/agus/gamejournal/GameJournalApplication.java
+++ src/main/java/agus/gamejournal/GameJournalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GameListApplication {
+public class GameJournalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GameListApplication.class, args);
+        SpringApplication.run(GameJournalApplication.class, args);
     }
 
 }
